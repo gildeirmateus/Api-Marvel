04b0a0f6b54214c3818a9e3d32b53dec8664ca7c
gildeir <gildeirmateus.com@gmail.com>
2021-06-26 22:38:33 -0300

    Change Api message

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const swaggerFile = require('../swagger_output.json')
 
 require("dotenv").config()
 
+app.get('/', (req, res)=>{
+    res.send('Hello from GitHub Action')
+})
+
 app.use(express.json())
 app.use('/', router)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
